4362305aba7bd3eb939eed6991365bf32d2a7b62
mahdi-samadi <mahdisamaadi@gmail.com>
2024-03-11 11:25:03 +0330

    add session 9 part 2

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,13 +1,29 @@
-let js = "amazing";
-if (js === "amazing") alert("javascript is Fund!!");
+// let js = "amazing";
+// if (js === "amazing") alert("javascript is Fund!!");
 
-console.log(50 + 15 + 66 + 11 - 9);
+// console.log(50 + 15 + 66 + 11 - 9);
 
+// let myNames = "mahdi";
+// console.log(myNames);
+// console.log(typeof myNames);
 
-let myNames = "mahdi";
-console.log(myNames);
-console.log(typeof myNames);
+// myNames = 13;
+// console.log(myNames);
+// console.log(typeof myNames);
 
-myNames = 13;
-console.log(myNames);
-console.log(typeof myNames);
+// var myNumber = 4343;
+// console.log(myNumber);
+
+// mahdi = "samadi";  // behtare ke injuri bdone tarife var / let / const/ moteqayer nasazim
+// console.log(mahdi);
+
+const now = 2025;
+const ageMahdi = now - 1995;
+const ageElnaz = now - 1994;
+console.log(ageElnaz * 2, ageMahdi / 3, 2 * 2.4);
+
+console.log(ageElnaz > ageMahdi);
+console.log(ageElnaz >= 40);
+
+const inFullAge = ageElnaz >= 45;
+console.log(inFullAge / 2, inFullAge);
