401e227c6977f99c8efdb8f0bbd7f07a0cf88ce2
mahdi-samadi <mahdisamaadi@gmail.com>
2024-04-01 21:13:04 +0330

    add some excercise

--- 02-JavaScriptFundamentals-Part1/script.js
+++ 02-JavaScriptFundamentals-Part1/script.js
@@ -217,10 +217,25 @@
 
 'use strict';
 
-// Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+// Data needed for a later exercise
 
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [`${weekdays[5]}`]: {
+    open: 0, // Open 24 hours
+    close: 24 / 3,
+  },
+};
 // Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
@@ -228,37 +243,35 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-
-  order: function (starterIndex, mainIndex) {
+  openingHours,
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
-
-  orderDelivery: function ({
-    starterIndex = 1,
-    mainIndex = 0,
-    time = '20:20',
-    address,
-  }) {
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:20', address }) {
     console.log(
       `order recived : ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} at ${time} to ${address}`
     );
   },
+  orderPizza(mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for (const day of days) {
+  // console.log(day);
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  // console.log(`on ${day}, we open at ${open}`);
+}
+console.log(restaurant.order?.(0, 1) ?? 'method does not exist');
+console.log(restaurant.orderrisstoo?.(0, 1) ?? 'method does not exist');
 
+const entries = Object.entries(openingHours);
+console.log(entries);
+console.log(openingHours);
+
+// console.log(restaurant.openingHours);
+// if (restaurant.openingHours.fri) console.log(restaurant.openingHours.fri.open);
 // restaurant.orderDelivery({
 //   time: '22:30',
 //   address: 'via del sole, 21',
