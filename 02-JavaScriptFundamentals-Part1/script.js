8057b3b9f06b5ac59c21150ef06f9d4d5dca356f
mahdi-samadi <mahdisamaadi@gmail.com>
2024-04-02 14:59:24 +0330

    final challenged 3

--- 02-JavaScriptFundamentals-Part1/script.js
+++ 02-JavaScriptFundamentals-Part1/script.js
@@ -215,60 +215,60 @@
 // [samadi, alinezhad] = [second, main];
 // console.log(samadi, alinezhad);
 
-'use strict';
+// 'use strict';
 
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-// Data needed for a later exercise
+// const flights =
+//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+// // Data needed for a later exercise
 
-const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
-const openingHours = {
-  [weekdays[3]]: {
-    open: 12,
-    close: 22,
-  },
-  [weekdays[4]]: {
-    open: 11,
-    close: 23,
-  },
-  [`${weekdays[5]}`]: {
-    open: 0, // Open 24 hours
-    close: 24 / 3,
-  },
-};
-// Data needed for first part of the section
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  openingHours,
-  order(starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
-  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:20', address }) {
-    console.log(
-      `order recived : ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} at ${time} to ${address}`
-    );
-  },
-  orderPizza(mainIngredient, ...otherIngredients) {
-    console.log(mainIngredient);
-    console.log(otherIngredients);
-  },
-};
-const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
-for (const day of days) {
-  // console.log(day);
-  const open = restaurant.openingHours[day]?.open ?? 'closed';
-  // console.log(`on ${day}, we open at ${open}`);
-}
-console.log(restaurant.order?.(0, 1) ?? 'method does not exist');
-console.log(restaurant.orderrisstoo?.(0, 1) ?? 'method does not exist');
+// const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+// const openingHours = {
+//   [weekdays[3]]: {
+//     open: 12,
+//     close: 22,
+//   },
+//   [weekdays[4]]: {
+//     open: 11,
+//     close: 23,
+//   },
+//   [`${weekdays[5]}`]: {
+//     open: 0, // Open 24 hours
+//     close: 24 / 3,
+//   },
+// };
+// // Data needed for first part of the section
+// const restaurant = {
+//   name: 'Classico Italiano',
+//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
+//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+//   openingHours,
+//   order(starterIndex, mainIndex) {
+//     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+//   },
+//   orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:20', address }) {
+//     console.log(
+//       `order recived : ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} at ${time} to ${address}`
+//     );
+//   },
+//   orderPizza(mainIngredient, ...otherIngredients) {
+//     console.log(mainIngredient);
+//     console.log(otherIngredients);
+//   },
+// };
+// const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+// for (const day of days) {
+//   // console.log(day);
+//   const open = restaurant.openingHours[day]?.open ?? 'closed';
+//   // console.log(`on ${day}, we open at ${open}`);
+// }
+// console.log(restaurant.order?.(0, 1) ?? 'method does not exist');
+// console.log(restaurant.orderrisstoo?.(0, 1) ?? 'method does not exist');
 
-const entries = Object.entries(openingHours);
-console.log(entries);
-console.log(openingHours);
+// const entries = Object.entries(openingHours);
+// console.log(entries);
+// console.log(openingHours);
 
 // console.log(restaurant.openingHours);
 // if (restaurant.openingHours.fri) console.log(restaurant.openingHours.fri.open);
@@ -321,3 +321,68 @@ console.log(openingHours);
 // restaurant.azam = null;
 // const guest = restaurant.azam ?? 'madhi';
 // console.log(guest);
+// const family = ['mahdi', 'azam', 'siyamak'];
+// const myFamily = new Set(family).size;
+// // myFamily.add('elnaz');
+// console.log(myFamily);
+// console.log(new Set('azam').size);
+
+// session 9 part 17
+const rest = new Map();
+rest.set('name', 'Mahdi');
+rest.set('name', 'Mahdis');
+rest.set(1, 'samadi', 'Iranian');
+rest.set('categories', ['Italian', 'Pizzeria', 'vegeterian', 'Organic']);
+rest.set('open', 11);
+rest.set('close', 23);
+rest.set(true, 'we are open :D');
+rest.set(false, 'we are close :(');
+
+console.log(rest.size);
+
+const task = {
+  task: 'code',
+  code: 'taskam',
+  100: 'to',
+  true: 'mahdi',
+  '': 'samadi',
+};
+console.log(task);
+// console.log([...rest]); //SAME  // console.log([...rest.entries()]);
+// console.log([...rest.keys()]);
+// console.log(rest.values());
+// console.log(rest.get(true));
+
+// const time = 114;
+// console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+// console.log(rest.has(1));
+// rest.delete(1);
+// console.log(rest);
+// // rest.clear();
+// rest.set([1, 2], 'test');
+// console.log(rest);
+
+// session 9 part 18
+// const question = new Map([
+//   ['question', "what's the best language programing in the world???"],
+//   [1, 'C'],
+//   [2, 'java'],
+//   [3, 'javascript'],
+//   ['correct', 3],
+//   [true, 'correct😍'],
+//   [false, 'try again'],
+// ]);
+// console.log(question.get('question'));
+// for (const [key, value] of question) {
+//   if (typeof key === 'number') console.log(`Aswer ${key} : ${value}`);
+// }
+// const answer = Number(prompt('your answer'));
+// console.log(answer);
+
+// if (answer == 3) {
+//   console.log(question.get(true));
+// } else {
+//   console.log(question.get(false));
+// }
+// OR ANSWER JONAS
+// console.log(question.get(question.get('correct') === answer));
--- 09-Data Structures, Modern Operators and Strings/script.js
+++ 09-Data Structures, Modern Operators and Strings/script.js
@@ -39,7 +39,40 @@ const game = {
   },
 };
 
-//CHALLENGED 2
+// CHALLENGED 3
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+// 1.
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// 2.
+gameEvents.delete(64);
+console.log(gameEvents);
+
+//3.
+const time = [...gameEvents.keys()].pop();
+console.log(
+  `An event happened, on average, every ${time / gameEvents.size} minutes`
+);
+
+// 4.
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] ${min}: ${event}`);
+}
+// CHALLENGED 2
 // 1.
 // for (const [i, player] of game.scored.entries())
 // console.log(`Gol ${i + 1}: ${player}`);
