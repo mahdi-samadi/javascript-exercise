467f84152f95d8d00e654af4ba7a540091040191
mahdi-samadi <mahdisamaadi@gmail.com>
2024-04-14 12:06:57 +0330

    final episode 26

--- 11-/script.js
+++ 11-/script.js
@@ -61,9 +61,12 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovement = function (movements) {
+const displayMovement = function (movements, sort = false) {
   containerMovements.innerHTML = '';
-  movements.forEach(function (mov, i) {
+
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const html = `
       <div class="movements__row">
@@ -201,6 +204,13 @@ btnClose.addEventListener('click', function (e) {
   }
   inputCloseUsername.value = inputClosePin.value = '';
 })
+
+let sorted = false
+btnSort.addEventListener('click', function(e) {
+  e.preventDefault();
+  displayMovement(currentAccount.movements, !sorted);
+  sorted = !sorted;
+});
 /////////////////////////////////////////////
 /////////////////////////////////////////////
 // LECTURES
@@ -289,3 +299,11 @@ btnClose.addEventListener('click', function (e) {
 // };
 // const age1 = calcAverageHumanAge([20, 2, -400, 3000, -650, -130, 70, 1300]);
 // console.log(age1);
+
+
+
+
+
+
+// const z = Array.from({length: 7} , (cur,i) => i+5);
+// console.log(z);
